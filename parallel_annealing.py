3b85779c601025245d5f253d7cefc2f8adc9708b
Tomek Matuszek <tom.mateuszek@gmail.com>
2023-05-20 16:11:44 +0200

    added resolution parameter

--- parallel_annealing.py
+++ parallel_annealing.py
@@ -1,9 +1,9 @@
 from algorithms import LocationAnnealing
 import warnings
-warnings.simplefilter(action='ignore', category=UserWarning)
 
-def parallel_annealing(data, iter, init='highest', move_choice='random', neighbourhood=2, n_shops=6, buffer=1500,
-                       objective=None, n_evals=None, n_rejected=None, start_temp=0.001, temp_mult=None, temp_substr=None):
-    la = LocationAnnealing(data)
-    res = la.run(init, move_choice, neighbourhood, n_shops, buffer, objective, n_evals, n_rejected, start_temp, temp_mult, temp_substr)
-    return (iter, res, (init, move_choice, neighbourhood, n_shops, buffer, objective, start_temp, temp_mult, temp_substr))
+def parallel_annealing(data, resolution, iter, init='highest', move_choice='random', neighbourhood=2, n_shops=6, buffer=1500,
+                       objective=None, n_evals=None, prop_rejected=None, start_temp=0.001, temp_mult=None, temp_substr=None):
+    warnings.simplefilter(action='ignore', category=UserWarning)
+    la = LocationAnnealing(data, resolution)
+    res, status = la.run(init, move_choice, neighbourhood, n_shops, buffer, objective, n_evals, prop_rejected, start_temp, temp_mult, temp_substr)
+    return (iter, res, (init, move_choice, neighbourhood, n_shops, buffer, objective, n_evals, prop_rejected, start_temp, temp_mult, temp_substr))
