60936e1b3592e50e2e56470abcbefc9f309b2b00
Tomek Matuszek <tom.mateuszek@gmail.com>
2023-05-21 16:56:59 +0200

    number of iterations as result

--- parallel_annealing.py
+++ parallel_annealing.py
@@ -4,5 +4,5 @@ import warnings
 def parallel_annealing(data, resolution, iter, params:Parameters):
     warnings.simplefilter(action='ignore', category=UserWarning)
     la = LocationAnnealing(data, resolution)
-    res, status = la.run(params)
+    res, evals, status = la.run(params)
     return (iter, res, params.values)
