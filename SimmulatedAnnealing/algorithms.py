a91457a2efc77d9a4b8b5358045978cf1584c073
Tomek Matuszek <tom.mateuszek@gmail.com>
2023-05-24 01:47:29 +0200

    added weights to sampling parents

--- SimmulatedAnnealing/algorithms.py
+++ SimmulatedAnnealing/algorithms.py
@@ -223,7 +223,9 @@ class LocationEvolution(OptimizationAlgorithm):
         self.maxs = []
     def choose_parents(self) -> 'list[gpd.GeoDataFrame]':
         while True:
-            samples = list(np.random.choice(range(0, len(self.population)), size=2, replace=False))
+            base_weights = self.scores - min(self.scores)
+            weights = base_weights / sum(base_weights)
+            samples = list(np.random.choice(range(0, len(self.population)), size=2, replace=False, p=weights))
             parents = [self.population[i] for i in samples]
             if not bool(set(parents[0].index) & set(parents[1].index)):
                 break
